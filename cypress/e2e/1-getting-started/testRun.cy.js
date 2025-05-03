64464ab24996032d3440081dc41bda100605da13
Chandreyee <150751077+chandreyee729@users.noreply.github.com>
2025-05-03 03:16:40 +0200

    Delete cypress/e2e/1-getting-started directory
    removed 1-getting-started directory

--- cypress/e2e/1-getting-started/testRun.cy.js
+++
@@ -1,8 +0,0 @@
-console.log('✅ File is loading');
-
-describe('Sanity Test', () => {
-  it('should work', () => {
-    cy.visit('https://naveenautomationlabs.com/opencart/index.php?route=account/login')
-    expect(true).to.equal(true);
-  });
-});
