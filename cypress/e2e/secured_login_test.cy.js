7652bcd43624baa386f60eb2a7269fccb0a5fead
chandreyee729 <chandreyeechakrabortyster@gmail.com>
2025-05-14 23:15:16 +0200

    fixed the cypress.env.json

--- cypress/e2e/secured_login_test.cy.js
+++ cypress/e2e/secured_login_test.cy.js
@@ -8,15 +8,18 @@ const {login_url} = routes;
 const registeredEmail = Cypress.env('email');
 const password = Cypress.env('user_password');
 
-describe('Login to a secured account', ( ) => {
+describe('Login to a secured account from Github Actions Secrets Workflow', ( ) => {
 
     beforeEach(() => {
         cy.visit(login_url);
+        cy.log(`email is ${registeredEmail}`)
     })
 
-    it(`Registered user ${registeredEmail} logs in successfully to account`, { tags: ['secured'] },() => {
+    it(`Github Actions Secrets overrides user password over Cypresss Env for ${registeredEmail}`, { tags: ['secured'] },() => {
         login.enterEmail(registeredEmail);
+        cy.log(`${registeredEmail} entered succesfully`)
         login.enterPassword(password);
+        cy.log("password from CI entered succesfully");
         login.submit().should('contain','Login');
         login.submit().click();
         cy.get('#content').contains('My Account');
